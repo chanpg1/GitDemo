eb15fa7816f36e6c89790a3d88bcec84ded23b25
Jackie <Jackie@JC-Acer5750>
2012-09-11 19:36:59 -0700

    Initial Commit

---
+++ src/ubc/cpsc410/gitdemo/MainClass.java
@@ -0,0 +1,14 @@
+package ubc.cpsc410.gitdemo;
+
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("I don't Git this...");
+
+	}
+
+}
